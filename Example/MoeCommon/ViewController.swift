5bf05e833d9f72a0cc3581ad3f818c074317b915
Zed <zed@moemoetech.com>
2019-11-19 11:25:21 +0800

    项目初始化

--- Example/MoeCommon/ViewController.swift
+++ Example/MoeCommon/ViewController.swift
@@ -7,17 +7,15 @@
 //
 
 import UIKit
+import MoeCommon
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let result = "Hello".moe.subString(start: 2)
+        print(result)
     }
 
 }
